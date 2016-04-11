8d345577e8194d215272d9c658befce0ad5b5a6c
Wan20 <wandy20number@gmail.com>
2016-04-11 22:51:54 +0700

    Revision

--- app/src/main/java/com/example/wansu/reverse/MainActivity.java
+++ app/src/main/java/com/example/wansu/reverse/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.wansu.reverse;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -26,16 +24,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        getSupportActionBar().setTitle("Reverse");
         edtInput = (EditText) findViewById(R.id.edt_input);
         btnReverse = (Button) findViewById(R.id.btn);
         txtHasil = (TextView) findViewById(R.id.txt_hasil);
